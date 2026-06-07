897f9a06466231b6c7741258dc73837fc32b437e
01rasmus <68335712+01rasmus@users.noreply.github.com>
2026-06-07 17:40:19 +0200

    update: to new rlsl and fix 404 page

--- site/src/main.tsx
+++ site/src/main.tsx
@@ -7,6 +7,7 @@ import Header, { HEADER_HEIGHT } from './components/Header.tsx'
 import Playground, { PLAYGROUND_ROUTE } from './routes/Playground.tsx'
 import Home, { HOME_ROUTE } from './routes/Home.tsx'
 import Documentation, { DOCUMENTATION_ROUTE } from './routes/Documentation.tsx'
+import NotFound from './routes/NotFound.tsx'
 
 const RouteContainer = styled.div`
   height: calc(100vh - ${HEADER_HEIGHT});
@@ -22,6 +23,7 @@ createRoot(document.getElementById('root')!).render(
           <Route path={HOME_ROUTE} element={<Home />} />
           <Route path={PLAYGROUND_ROUTE} element={<Playground />} />
           <Route path={DOCUMENTATION_ROUTE} element={<Documentation />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </RouteContainer>
     </BrowserRouter>
